308e10e45106b2897be824fd98982e3bb57ec31f
Rebeca <rebeca.felix@hotmail.com>
2019-04-23 18:49:45 -0300

    function multiply and add array, done

--- card-validator.js
+++ card-validator.js
@@ -1,35 +1,26 @@
 function cardValidator(number) {
-  let lengthNumber = number.toString()
-  let result =[];
+  const lengthNumber = number.toString()
+  const result =[];
 
   for(i in lengthNumber){
-    // console.log(i)
-    
-    if(i % 2 === 0){
-      let multiplyIndex = lengthNumber[i] * 2;
-      // result.push(multiplyIndex);
 
-        if(multiplyIndex > 9){
-          result.push(multiplyIndex -9);
-        }
+    if(i % 2 === 0){
+      const multiplyIndex = lengthNumber[i] * 2;
+      
+      multiplyIndex > 9 ? result.push(multiplyIndex -9) : result.push(multiplyIndex);
+
     }else{
       result.push(Number(lengthNumber[i]));
     };
   };
-  console.log(result)
-};
-cardValidator(54329)
-
-
-//soma de todos os nºs % 10
-
-// //função de somar array
-
-// for(i in array){
-//   let soma = array.reduce(function(anterior,atual){
-//     return anterior + atual;
-
-//   })
-// console.log(soma)
   
-// }
+  const sumAll = result.reduce((acc,cur) => acc + cur)
+
+  return sumAll % 10 === 0 ? console.log("cartão válido") : console.log("cartão inválido") 
+};
+// cardValidator(5502098769227998)
+
+let card = require("./lib/card-validator")
+module.exports=card
+
+
