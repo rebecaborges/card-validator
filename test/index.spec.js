d8b70fea79af600f48f4680f5896b51fc5f80359
Rebeca <rebeca.felix@hotmail.com>
2019-04-26 16:36:22 -0300

    test done closes #5

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,23 +4,29 @@ let expect = chai.expect;
 let lib = require("../lib/index");
 
 describe("cardValidator()", ()=> {
-   it("deve retornar cartao inválido", ()=> {
+   it("para numeros aleatórios retorna false", ()=> {
     expect(lib.cardValidator("02029330")).to.equal(false)
    });
-   it("deve retornar cartão inválido", ()=> {
+   it("para campo vazio do tipo string retorna false", ()=> {
     expect(lib.cardValidator("")).to.equal(false)
   });
-  it("deve retornar cartão inválido", ()=> {
+  it("para numero em string repetida retorna false", ()=> {
     expect(lib.cardValidator("000000000000000000")).to.equal(false)
   });
-  it("deve retornar cartão inválido", ()=> {
+  it("para campo com string vazio retorna falso", ()=> {
+    expect(lib.cardValidator("         ")).to.equal(false)
+  });
+  it("para campo com espacos vazio retorna falso", ()=> {
     expect(lib.cardValidator(         )).to.equal(false)
   });
-  it("deve retornar cartão inválido", ()=> {
+  it("para campo vazio retorna falso", ()=> {
     expect(lib.cardValidator()).to.equal(false)
   });
-   it("deve retornar cartão válido", ()=> {
-    expect(lib.cardValidator("5502098769227998")).to.equal(true)
+   it("para campo com numeros aleatorios retorna falso", ()=> {
+    expect(lib.cardValidator(0000000011223)).to.equal(false)
+  });
+  it("deve retornar true para cartão válido", ()=> {
+    expect(lib.cardValidator(5555666677778884)).to.equal(true)
   });
   
 });
